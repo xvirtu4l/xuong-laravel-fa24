c560f8136697472523b2a617e61abe22ea7c18f5
LÊ NGOC MINH <minhle1309@gmail.com>
2024-10-14 14:54:33 +0700

    route fix

--- routes/web.php
+++ routes/web.php
@@ -108,23 +108,23 @@ Route::group(['middleware' => 'RoleMiddleware'], function () {
     Route::resource('employees', EmployeeController::class);
     Route::delete('employees/{employee}/forceDestroy', [EmployeeController::class, 'forceDestroy'])->name('employees.forceDestroy');
     Route::post('employees/{employee}/restore', [EmployeeController::class, 'restore'])->name('employees.restore');
-    
+
     Route::get('/students/search', [StudentController::class, 'search'])->name('students.search');
     Route::resource('students', StudentController::class);
 
     Route::resource('classrooms', ClassroomController::class);
     Route::resource('subjects', SubjectController::class);
+
+    Route::resource('sql', SQLhw::class);
+
+    Route::get('/movies', function () {
+        return view('movies');
+    })->middleware('checkAge');
+
+    Route::middleware(['transaction'])->group(function () {
+        Route::get('/start-transaction', [TransactionController::class, 'startTransaction'])->name('start-transaction');
+        Route::post('/confirm-transaction', [TransactionController::class, 'confirmTransaction'])->name('confirm-transaction');
+        Route::post('/complete-transaction', [TransactionController::class, 'completeTransaction'])->name('complete-transaction');
+        Route::post('/cancel-transaction', [TransactionController::class, 'cancelTransaction'])->name('cancel-transaction');
+    });
 });
-
-Route::middleware(['transaction'])->group(function () {
-    Route::get('/start-transaction', [TransactionController::class, 'startTransaction'])->name('start-transaction');
-    Route::post('/confirm-transaction', [TransactionController::class, 'confirmTransaction'])->name('confirm-transaction');
-    Route::post('/complete-transaction', [TransactionController::class, 'completeTransaction'])->name('complete-transaction');
-    Route::post('/cancel-transaction', [TransactionController::class, 'cancelTransaction'])->name('cancel-transaction');
-});
-
-Route::resource('sql', SQLhw::class);
-
-Route::get('/movies', function () {
-    return view('movies');
-})->middleware('checkAge');
